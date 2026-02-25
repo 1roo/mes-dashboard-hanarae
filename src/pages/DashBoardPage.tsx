e83f23370215a1237fd7a211919e445f246fc334
1roo <93hanaru@gmail.com>
2026-02-25 09:16:01 +0900

    로그인시 대시보드에서 관리자만 계정관리 버튼 보이게 수정

--- src/pages/DashBoardPage.tsx
+++ src/pages/DashBoardPage.tsx
@@ -1,5 +1,22 @@
+import { useNavigate } from "react-router-dom";
+
 const DashBoardPage = () => {
-  return <div>DashBoardPage</div>;
+  const navigate = useNavigate();
+  const role = localStorage.getItem("role");
+
+  return (
+    <div>
+      {role === "ADMIN" && (
+        <button
+          onClick={() => navigate("/users")}
+          className="border border-blue-300 rounded-md bg-blue-100 p-2 hover:bg-blue-200"
+        >
+          계정관리
+        </button>
+      )}
+      <p>dashboard</p>
+    </div>
+  );
 };
 
 export default DashBoardPage;
